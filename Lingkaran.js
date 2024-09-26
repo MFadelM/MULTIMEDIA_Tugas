315ce53ae7c7279868d8fb4e93d446ffa435c364
MFadelM <136667227+MFadelM@users.noreply.github.com>
2024-09-26 10:03:20 +0700

    Create Lingkaran.js

---
+++ Lingkaran.js
@@ -0,0 +1,12 @@
+// mohammad fadel mas'an;
+// 22104410117
+function setup() {
+  createCanvas(640, 480);
+}
+
+function draw() {
+  background(0);
+  fill(0, 255, 0);
+  noStroke();
+  ellipse(width / 2, height / 2, 200, 200); 
+}
