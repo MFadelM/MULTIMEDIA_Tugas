a313e480d269129ab2692720ce4868a5b35d8af0
MOHAMMAD FADEL MAS'AN <136667227+MFadelM@users.noreply.github.com>
2024-09-26 23:13:51 +0700

    Create Tugas.js

---
+++ Tugas.js
@@ -0,0 +1,13 @@
+// mohammad fadel mas'an;
+// 22104410117
+function setup() {
+  createCanvas(640, 480);
+}
+
+function draw() {
+  background(0);
+  fill(0, 255, 0);
+  noStroke();
+  ellipse(width / 2, height / 2, 200, 200);
+  rect(123,123,123);
+}
